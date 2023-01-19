01ba3f74777ae44e58b9668a47295de96600589d
Destrarion <Destarion@gmail.com>
2023-01-19 13:13:04 +0100

    Initial Commit

---
+++ githubactiontest/githubactiontestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubactiontestApp.swift
+//  githubactiontest
+//
+//  Created by Fabien Dietrich on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct githubactiontestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
